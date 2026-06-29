79d09d0141086e67e912e06cbe0505c99cb13368
mcrgnt <microgiant@yandex.ru>
2026-06-30 00:07:47 +0300

    add CatalogSegment api func (#5)

--- doc.go
+++ doc.go
@@ -3,5 +3,7 @@
 // Process defaults: env prefix [DefaultPrefix], custom tag key [DefaultTagKey].
 // Override with [SetPrefix] and [SetTagKey] before [LoadEnv].
 //
+// [CatalogSegment] reads the catalog wire field tag named [TagKey].
+//
 // Standalone struct parsing without a registry is [github.com/omcrgnt/ecfg/config.Parse].
 package ecfg
